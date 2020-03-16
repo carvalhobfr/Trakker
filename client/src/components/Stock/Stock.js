85c2ab312f80c533154ce66089b381a5ed754f5b
Matheus Carvalho <carvalho.bfr@gmail.com>
2020-03-16 12:15:29 +0000

    Stock component

---
+++ client/src/components/Stock/Stock.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+var alpha = require('alphavantage')({ key: 'JGVPFO4GLBB9BIUO' });
+
+const Stock = props => (
+	<div>
+		<div>
+			<p>
+				Name: {props.name} Price: {props.price} Number: {props.num} Total: {props.total}
+			</p>
+		</div>
+	</div>
+);
+
+getPrice = (name, num) => {
+	alpha.data.daily(name).then(data => {
+		const imeSeries = data['Time Series (Daily)'];
+		const today = Object.values(imeSeries)[0];
+		const price = today['4. close'];
+		const total = price * num;
+		this.setState(() => ({
+			price: price,
+			total: total
+		}));
+	});
+};
+
+export default Stock;
