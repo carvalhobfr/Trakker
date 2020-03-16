362cd9c0122242bc6bc70355d5be9d58b70cf3bb
Matheus Carvalho <carvalho.bfr@gmail.com>
2020-03-16 12:07:58 +0000

    new service api

--- server/routes/stocks.js
+++ server/routes/stocks.js
@@ -5,41 +5,42 @@ const { Router } = require('express');
 //const User = require('./../models/user');
 const Wallet = require('./../models/wallet');
 const Stock = require('./../models/stock');
+const alpha = require('alphavantage')({ key: '{{RWIQ5QDC9KY8INZG}}' });
 
 const router = new Router();
 
 router.post('/add-stock', (req, res, next) => {
-  const userID = req.user._id;
-  const { name, type, quantity, buying_price, currency, date_of_purchase } = req.body;
-  Wallet.findOne({ user: userID })
-    .then(wallet => {
-      return Stock.create({
-        name,
-        type,
-        quantity,
-        buying_price,
-        currency,
-        date_of_purchase,
-        wallet
-      });
-    })
-    .then(() => {
-      res.redirect('/');
-    })
-    .catch(error => {
-      next(error);
-    });
+	const userID = req.user._id;
+	const { name, type, quantity, buying_price, currency, date_of_purchase } = req.body;
+	Wallet.findOne({ user: userID })
+		.then(wallet => {
+			return Stock.create({
+				name,
+				type,
+				quantity,
+				buying_price,
+				currency,
+				date_of_purchase,
+				wallet
+			});
+		})
+		.then(() => {
+			res.redirect('/');
+		})
+		.catch(error => {
+			next(error);
+		});
 });
 
 router.get('/stock-information/:id', (req, res, next) => {
-  const stockId = req.params.id;
-  Stock.findById(stockId)
-    .then(stock => {
-      res.json({ stock });
-    })
-    .catch(error => {
-      next(error);
-    });
+	const stockId = req.params.id;
+	Stock.findById(stockId)
+		.then(stock => {
+			res.json({ stock });
+		})
+		.catch(error => {
+			next(error);
+		});
 });
 
 module.exports = router;
