6fa4ff960266f2dfa5aea6a3bf824ae44a0e6b27
Felipe Lima <felipenplima@gmail.com>
2020-03-16 16:06:42 +0000

    homepage styling

--- client/src/views/HomePageView/index.jsx
+++ client/src/views/HomePageView/index.jsx
@@ -5,9 +5,33 @@ import './style.scss';
 class HomePageView extends Component {
   render() {
     return (
-      <div className="Homepage">
-        <h1>Control all your assets in one place</h1>
-      </div>
+      <body>
+        <header className="header">
+          <div>
+            <h1>
+              <strong>Control all your assets in one place</strong>
+            </h1>
+            <h4 className="secondary-text">
+              Trakker is a portfolio manager that unifies all your crypto and stocks shares in one
+              place.
+            </h4>
+            <button className="signin-button">
+              <Link to="/sign-in">Sign In</Link>
+            </button>
+          </div>
+        </header>
+        <section className="mockups">
+          <div className="mockup1">
+            <h3>
+              <strong>Manage your portfolio like never before</strong>
+            </h3>
+            <h5>
+              Fatback excepteur est sed nulla nisi. Swine eu pariatur shoulder ut culpa. In ut anim
+              salami. Ullamco flank elit veniam lorem boudin.
+            </h5>
+          </div>
+        </section>
+      </body>
     );
   }
 }
