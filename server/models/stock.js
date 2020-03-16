b07a60d73276ba384d87e5544ac5ab8c25a58751
Matheus Carvalho <carvalho.bfr@gmail.com>
2020-03-16 16:46:45 +0000

    router problem

--- server/models/stock.js
+++ server/models/stock.js
@@ -3,30 +3,31 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-  name: {
-    type: String
-  },
-  type: {
-    type: String,
-    enum: ['Stock', 'Crypto']
-  },
-  quantity: {
-    type: Number
-  },
-  buying_price: {
-    type: Number
-  },
-  currency: {
-    type: String,
-    default: 'USD'
-  },
-  wallet: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Wallet'
-  },
-  date_of_purchase: {
-    type: Date
-  }
+	name: {
+		type: String
+	},
+	type: {
+		type: String,
+		enum: ['Stock', 'Crypto'],
+		required: true
+	},
+	quantity: {
+		type: Number
+	},
+	buying_price: {
+		type: Number
+	},
+	currency: {
+		type: String,
+		default: 'USD'
+	},
+	wallet: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Wallet'
+	},
+	date_of_purchase: {
+		type: Date
+	}
 });
 
 module.exports = mongoose.model('Stock', schema);
