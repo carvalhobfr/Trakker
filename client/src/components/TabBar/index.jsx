ac5f0bed1fcc535e5dce7c83cb496d1d6bee2ad7
Matheus Carvalho <carvalho.bfr@gmail.com>
2020-03-17 12:54:51 +0000

    tab bar work

--- client/src/components/TabBar/index.jsx
+++ client/src/components/TabBar/index.jsx
@@ -7,23 +7,20 @@ const tabs = document.querySelectorAll('.tab');
 const TabBar = props => {
 	return (
 		<div class="tab-nav-container">
-			<div class="tab active purple">
+			<div class="tab purple">
 				<i class="fas fa-home"></i>
-				<p>Home</p>
 			</div>
 			<div class="tab pink">
-				<i class="far fa-heart"></i>
-				<p>Likes</p>
+				<i class="fas fa-euro-sign"></i>
 			</div>
 			<div class="tab yellow">
 				<i class="fas fa-chart-bar"></i>
-				<p>Charts</p>
+
 			</div>
 			<div class="tab teal">
 				<i class="fas fa-cog"></i>
-				<p>Setting</p>
 			</div>
-		</div>
+		</div >
 	);
 };
 
