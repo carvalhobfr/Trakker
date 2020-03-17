57153acd933e41d83b472c39e5244d8672957b61
Felipe Lima <felipenplima@gmail.com>
2020-03-17 17:12:24 +0000

    TabBar routes working

--- client/src/components/TabBar/index.jsx
+++ client/src/components/TabBar/index.jsx
@@ -3,22 +3,26 @@ import { Link } from 'react-router-dom';
 import './style.scss';
 
 const TabBar = props => {
-	return (
-		<div class="tab-nav-container">
-			<div class="tab">
-				<i class="fas fa-home"></i>
-			</div>
-			<div class="tab">
-				<i class="fas fa-euro-sign"></i>
-			</div>
-			<div class="tab">
-				<i class="fas fa-chart-bar"></i>
-			</div>
-			<div class="tab">
-				<i class="fas fa-cog"></i>
-			</div>
-		</div >
-	);
+  return (
+    <div class="tab-nav-container">
+      <div class="tab-purple">
+        <i class="fas fa-home"></i>
+        <Link to="/dashboard" class="fas fa-home"></Link>
+      </div>
+      <div class="tab-pink">
+        <i class="fas fa-euro-sign"></i>
+        <Link to="/wallet" class="fas fa-cog"></Link>
+      </div>
+      <div class="tab-yellow">
+        <i class="fas fa-chart-bar"></i>
+        <Link to="/allstocks" class="fas fa-cog"></Link>
+      </div>
+      <div class="tab-teal">
+        <i class="fas fa-cog"></i>
+        <Link to="/settings" class="fas fa-cog"></Link>
+      </div>
+    </div>
+  );
 };
 
 export default TabBar;
