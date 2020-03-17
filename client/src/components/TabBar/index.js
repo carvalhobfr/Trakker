3a7cdff087c57f291862b5bf9861d107ec9fd65b
Matheus Carvalho <carvalho.bfr@gmail.com>
2020-03-17 12:10:37 +0000

    tab bar problems

---
+++ client/src/components/TabBar/index.js
@@ -0,0 +1,10 @@
+const tabs = document.querySelectorAll('.tab');
+
+tabs.forEach(clickedTab => {
+  clickedTab.addEventListener('click', () => {
+    tabs.forEach(tab => {
+      tab.classList.remove('active');
+    });
+    clickedTab.classList.add('active');
+  });
+});
