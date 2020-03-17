2629174d7724ef2e303473a68b34e0755dbce281
Matheus Carvalho <carvalho.bfr@gmail.com>
2020-03-17 14:58:56 +0000

    reduce in stocks

--- server/routes/stocks.js
+++ server/routes/stocks.js
@@ -9,69 +9,70 @@ const Stock = require('./../models/stock');
 const router = new Router();
 
 router.post('/add-stock', async (req, res, next) => {
-  console.log(req.body);
-  const {
-    name,
-    type,
-    quantity,
-    buying_price,
-    currency,
-    date_of_purchase,
-    user,
-    wallet
-  } = req.body.data;
-  try {
-    //const wallet = await Wallet.findOne({ user: user._id });
-    const stock = await Stock.create({
-      name,
-      type,
-      quantity,
-      buying_price,
-      currency,
-      date_of_purchase,
-      wallet
-    });
-    res.json({ stock });
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
+	console.log(req.body);
+	const {
+		name,
+		type,
+		quantity,
+		buying_price,
+		currency,
+		date_of_purchase,
+		user,
+		wallet
+	} = req.body.data;
+	try {
+		//const wallet = await Wallet.findOne({ user: user._id });
+		const totalAmount = buying_price.reduce((total, buying_price) => total + buying_price, 0);
+		const stock = await Stock.create({
+			name,
+			type,
+			quantity,
+			buying_price: totalAmount / stock.length,
+			currency,
+			date_of_purchase,
+			wallet
+		});
+		res.json({ stock });
+	} catch (error) {
+		console.log(error);
+		next(error);
+	}
 });
 
 router.get('/allstocks/:id', async (req, res, next) => {
-  const walletID = req.params.id;
-  try {
-    const stocks = await Stock.find({ wallet: walletID });
-    res.json({ stocks });
-    console.log('BACKEND ', stocks);
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
+	const walletID = req.params.id;
+	try {
+		const stocks = await Stock.find({ wallet: walletID });
+		res.json({ stocks });
+		console.log('BACKEND ', stocks);
+	} catch (error) {
+		console.log(error);
+		next(error);
+	}
 });
 
 router.get('/singlestock/:id', async (req, res, next) => {
-  const stockID = req.params.id;
-  try {
-    const stock = await Stock.findById({ stockID });
-    res.json({ stock });
-    console.log('BACKEND ', stock);
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
+	const stockID = req.params.id;
+	try {
+		const stock = await Stock.findById({ stockID });
+		res.json({ stock });
+		console.log('BACKEND ', stock);
+	} catch (error) {
+		console.log(error);
+		next(error);
+	}
 });
 
 router.get('/:id', async (req, res, next) => {
-  console.log(req.params.id);
-  const walletID = req.params.id;
-  console.log(walletID);
-  try {
-    const wallet = await Wallet.findById({ _id: walletID });
-    res.json({ wallet });
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
+	console.log(req.params.id);
+	const walletID = req.params.id;
+	console.log(walletID);
+	try {
+		const wallet = await Wallet.findById({ _id: walletID });
+		res.json({ wallet });
+	} catch (error) {
+		console.log(error);
+		next(error);
+	}
 });
 module.exports = router;
