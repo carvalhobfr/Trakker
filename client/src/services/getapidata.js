e1d7aa63d1fe2d669f5532fa9624a39583d8c463
Matheus Carvalho <carvalho.bfr@gmail.com>
2020-03-17 18:07:30 +0000

    api data

--- client/src/services/getapidata.js
+++ client/src/services/getapidata.js
@@ -5,11 +5,11 @@ import axios from 'axios';
 }); */
 
 const requestDaily = async name => {
-  const result = await axios.get(
-    `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${name}&apikey=${process.env.STOCK_API}`
-  );
-  const dailyStock = result.data;
-  console.log(dailyStock);
+	const result = await axios.get(
+		`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${process.env.STOCK_API}`
+	);
+	const dailyStock = result.data;
+	console.log(dailyStock);
 };
 
 export { requestDaily };
