e3d0dc0beceb822c5654fcdedba19aceabb629a4
Matheus Carvalho <carvalho.bfr@gmail.com>
2020-03-17 22:31:07 +0000

    improve in tabbar

--- client/src/components/TabBar/index.jsx
+++ client/src/components/TabBar/index.jsx
@@ -7,22 +7,18 @@ import './style.scss';
 const TabBar = props => {
   return (
     <div class="tab-nav-container">
-      <div class="tabIcon">
-        <i class="fas fa-home"></i>
-        <Link to="/dashboard" class="fas fa-home"></Link>
-      </div>
-      <div class="tabIcon">
+      <a href="/dashboard" class="linkIcon">
+        <i class="fas fa-home" />
+      </a>
+      <Link to="/wallet" class="linkIcon">
         <i class="fas fa-euro-sign"></i>
-        <Link to="/wallet" class="fas fa-cog"></Link>
-      </div>
-      <div class="tabIcon">
-        <i class="fas fa-chart-bar"></i>
-        <Link to="/allstocks" class="fas fa-cog"></Link>
-      </div>
-      <div class="tabIcon">
-        <i class="fas fa-cog"></i>
-        <Link to="/settings" class="fas fa-cog"></Link>
-      </div>
+      </Link>
+      <Link to="/allstocks" class="linkIcon">
+        <i class="fas fa-chart-bar" />
+      </Link>
+      <Link to="/settings" class="linkIcon">
+        <i class="fas fa-cog" />
+      </Link>
     </div>
   );
 };
