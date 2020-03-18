1c694929321c077e1f083c6a95640d530f87b329
Matheus Carvalho <carvalho.bfr@gmail.com>
2020-03-18 15:07:23 +0000

    first page solved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 //General imports
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 //Component imports
 import NavBar from './components/NavBar';
@@ -51,46 +51,46 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
-          <NavBar user={this.state.user} updateUserInformation={this.updateUserInformation} />
-          <Switch>
-            {/* <Route path="/" exact component={HomePageView} /> */}
-            <ProtectedRoute
-              path="/"
-              exact
-              authorized={!this.state.user}
-              redirect={'/dashboard'}
-              render={props => <HomePageView />}
-            />
-            <ProtectedRoute
-              path="/sign-up"
-              authorized={!this.state.user}
-              redirect={'/'}
-              render={props => (
-                <SignUpView {...props} updateUserInformation={this.updateUserInformation} />
-              )}
-            />
-            <ProtectedRoute
-              path="/sign-in"
-              authorized={!this.state.user}
-              redirect={'/'}
-              render={props => (
-                <SignInView {...props} updateUserInformation={this.updateUserInformation} />
-              )}
-            />
-            <ProtectedRoute
-              path="/add-stock"
-              authorized={this.state.user}
-              redirect={'/add-stock'}
-              render={props => (
-                <AddStockView
-                  {...props}
-                  user={this.state.user}
-                  updateUserInformation={this.updateUserInformation}
-                />
-              )}
-            />
-            {/*  <ProtectedRoute
+
+        <NavBar user={this.state.user} updateUserInformation={this.updateUserInformation} />
+        <Switch>
+          {/* <Route path="/" exact component={HomePageView} /> */}
+          <ProtectedRoute
+            path="/"
+            exact
+            authorized={!this.state.user}
+            redirect={'/dashboard'}
+            render={props => <HomePageView />}
+          />
+          <ProtectedRoute
+            path="/sign-up"
+            authorized={!this.state.user}
+            redirect={'/'}
+            render={props => (
+              <SignUpView {...props} updateUserInformation={this.updateUserInformation} />
+            )}
+          />
+          <ProtectedRoute
+            path="/sign-in"
+            authorized={!this.state.user}
+            redirect={'/'}
+            render={props => (
+              <SignInView {...props} updateUserInformation={this.updateUserInformation} />
+            )}
+          />
+          <ProtectedRoute
+            path="/add-stock"
+            authorized={this.state.user}
+            redirect={'/add-stock'}
+            render={props => (
+              <AddStockView
+                {...props}
+                user={this.state.user}
+                updateUserInformation={this.updateUserInformation}
+              />
+            )}
+          />
+          {/*  <ProtectedRoute
               path="/add-stock"
               authorized={this.state.user}
               redirect={'/dashboard'}
@@ -103,67 +103,66 @@ class App extends Component {
               )}
             /> */}
 
-            <ProtectedRoute
-              path="/dashboard"
-              authorized={this.state.user}
-              redirect={'/dashboard'}
-              render={props => (
-                <DashboardView
-                  {...props}
-                  user={this.state.user}
-                  wallet={this.state.user.wallet}
-                  updateUserInformation={this.updateUserInformation}
-                />
-              )}
-            />
+          <ProtectedRoute
+            path="/dashboard"
+            authorized={this.state.user}
+            redirect={'/dashboard'}
+            render={props => (
+              <DashboardView
+                {...props}
+                user={this.state.user}
+                wallet={this.state.user.wallet}
+                updateUserInformation={this.updateUserInformation}
+              />
+            )}
+          />
 
-            <ProtectedRoute
-              path="/singlestock"
-              authorized={this.state.user}
-              redirect={'/singlestock'}
-              render={props => (
-                <SingleOwnedStockView
-                  {...props}
-                  user={this.state.user}
-                  wallet={this.state.user.wallet}
-                  updateUserInformation={this.updateUserInformation}
-                />
-              )}
-            />
+          <ProtectedRoute
+            path="/singlestock"
+            authorized={this.state.user}
+            redirect={'/singlestock'}
+            render={props => (
+              <SingleOwnedStockView
+                {...props}
+                user={this.state.user}
+                wallet={this.state.user.wallet}
+                updateUserInformation={this.updateUserInformation}
+              />
+            )}
+          />
 
-            <ProtectedRoute
-              path="/wallet"
-              exact
-              authorized={this.state.user}
-              redirect={'/wallet'}
-              render={props => (
-                <WalletView
-                  {...props}
-                  wallet={this.state.user.wallet}
-                  user={this.state.user}
-                  updateUserInformation={this.updateUserInformation}
-                />
-              )}
-            />
+          <ProtectedRoute
+            path="/wallet"
+            exact
+            authorized={this.state.user}
+            redirect={'/wallet'}
+            render={props => (
+              <WalletView
+                {...props}
+                wallet={this.state.user.wallet}
+                user={this.state.user}
+                updateUserInformation={this.updateUserInformation}
+              />
+            )}
+          />
 
-            <ProtectedRoute
-              path="/settings"
-              exact
-              authorized={this.state.user}
-              redirect={'/settings'}
-              render={props => (
-                <SettingsView
-                  {...props}
-                  user={this.state.user}
-                  updateUserInformation={this.updateUserInformation}
-                />
-              )}
-            />
+          <ProtectedRoute
+            path="/settings"
+            exact
+            authorized={this.state.user}
+            redirect={'/settings'}
+            render={props => (
+              <SettingsView
+                {...props}
+                user={this.state.user}
+                updateUserInformation={this.updateUserInformation}
+              />
+            )}
+          />
 
-            {/* <Route path="/error" component={ErrorView} />
+          {/* <Route path="/error" component={ErrorView} />
                 <Redirect to="/error" /> */}
-          </Switch>
-        </BrowserRouter>
+        </Switch>
       </div>
     );
   }
