4d258cc612599e1191d0a78575ee02a4c6a70a99
Matheus Carvalho <carvalho.bfr@gmail.com>
2020-03-18 14:57:56 +0000

    last value stock

--- client/src/services/getapidata.js
+++ client/src/services/getapidata.js
@@ -19,9 +19,13 @@ const requestDaily = async name => {
 	const result = await axios.get(
 		`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${name}&apikey=${process.env.STOCK_API}`
 	);
-	const dailyStock = result.data;
-	// console.log('DailyStock em GetAPIDATA', dailyStock);
-	// console.log('DailyStock em GetAPIDATA', dailyStock."meta data".);
+  const dailyStock = result.data;
+  const info =  dailyStock["Time Series (Daily)"]
+  const firstKey = Object.keys(info)[0]
+  const data = info[firstKey]["4. close"]
+  console.log(data)
+  // console.log('DailyStock em GetAPIxDATA', dailyStock);
+	//console.log('DailyStock em GetAPIDATA', dailyStock["Time Series (Daily)"][0]);
 };
 
 export { requestDaily };
