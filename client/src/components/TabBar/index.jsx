3bbec4500970ccbd9fb6dffcffb1e76d099cdb38
Felipe Lima <felipenplima@gmail.com>
2020-03-19 16:24:14 +0000

    tab-bar wallet icon inclusion

--- client/src/components/TabBar/index.jsx
+++ client/src/components/TabBar/index.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import './style.scss';
+import { happy } from 'ionicons/icons';
 
 //tive que adicionar a classe dentro de <i> dentro de <Link> pra funcionar
 
@@ -8,20 +9,21 @@ const TabBar = props => {
   return (
     <div class="tab-nav-container">
       <a href="/dashboard" class="linkIcon">
-        <i class="fas fa-home" />
+        <i class="las la-home"></i>
       </a>
+      <ion-icon name="heart"></ion-icon>
 
       {/* <span style="font-size: 48px; color: Dodgerblue;">
         <i class="fas fa-camera"></i>
       </span> */}
       <Link to="/wallet" class="linkIcon">
-        <i class="fas fa-dollar-sign"></i>
+        <i class="las la-wallet"></i>
       </Link>
       <Link to="/allstocks" class="linkIcon">
-        <i class="fas fa-chart-bar" />
+        <i class="las la-chart-bar"></i>
       </Link>
       <Link to="/settings" class="linkIcon">
-        <i class="fas fa-cog" />
+        <i class="las la-user-cog"></i>
       </Link>
     </div>
   );
