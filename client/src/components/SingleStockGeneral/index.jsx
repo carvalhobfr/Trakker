e3f07db62efc1cf3c6398ee8de0607f4ade2748a
André Ferreira <mrawesomeferreira@gmail.com>
2020-03-23 15:42:04 +0000

    added toglle button

--- client/src/components/SingleStockGeneral/index.jsx
+++ client/src/components/SingleStockGeneral/index.jsx
@@ -7,6 +7,14 @@ import './style.scss';
 class SingleStockGeneral extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      toggle: false
+    };
+    this.changeToggle = this.changeToggle.bind(this);
+  }
+
+  changeToggle() {
+    this.setState({ toggle: !this.state.toggle });
   }
 
   render() {
@@ -21,7 +29,7 @@ class SingleStockGeneral extends Component {
         }}
         params={this.state.name}
       > */
-      <section className="stock__single-button">
+      <section className="stock__single-button" onClick={this.changeToggle}>
         <div className="stock__symbol">
           <h4>{this.props.single.name}</h4>
         </div>
@@ -31,6 +39,11 @@ class SingleStockGeneral extends Component {
           <p>{this.props.margin.toFixed(2)} % </p>
         </div>
         <hr />
+        {this.state.toggle && (
+          <a href="/add-stock" className="add__button">
+            Add to wallet
+          </a>
+        )}
       </section>
       // </Link>
     );
